8734756ed3f3ce63e5d65dfe82e90186b010f503
Cyrus Yncierto <mark.yncierto@rockpress.com.au>
2023-07-20 16:48:39 +0800

    initial commit, sequelize setup

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv"; // eslint-disable-line import/no-import-module-exports
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
